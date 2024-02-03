a965e111b43e1fb89947112b10cb3279548ed556
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2024-02-03 13:01:48 +0530

    change

--- myaestheticsolar/settings.py
+++ myaestheticsolar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t&vvhdj5npvgxn8u4ia419=e^w-ed+)%*opfc*c0n4kdgm6a%+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['54.206.213.196', 'www.myaestheticsolar.com', 'aestheticsoalr.com']
+ALLOWED_HOSTS = ['54.206.213.196', 'www.myaestheticsolar.com', 'aestheticsolar.com','127.0.0.1']
 
 
 # Application definition
